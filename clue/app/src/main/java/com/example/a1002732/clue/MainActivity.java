206391fffe2b9e73ccf6cf24f1dd4f5e0ae3af91
이준환 <junhwan_lee@sk.com>
2018-10-23 14:45:25 +0900

    CLUE 프로젝트 업로드

---
+++ clue/app/src/main/java/com/example/a1002732/clue/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.a1002732.clue;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Intent sensorIntent;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+
+    }
+}
---
+++ clue/app/src/main/java/com/example/a1002732/clue/SplashActivity.java
@@ -0,0 +1,25 @@
+package com.example.a1002732.clue;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+/**
+ * Created by 1002732 on 2018. 4. 17..
+ */
+
+public class SplashActivity extends Activity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        startActivity(new Intent(this, MainActivity.class));
+    }
+}
